fa07ff67e8c4fc5269bcb4f0825c494dcbca6a0e
aditya1290 <adityagarg1290@gmail.com>
2020-08-01 08:00:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sih2020_runtimeterror_mechanic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sih2020_runtimeterror_mechanic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
